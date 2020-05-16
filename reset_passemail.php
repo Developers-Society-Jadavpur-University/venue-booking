497472a99f90f8f8cbbb92530280eb34e3da845b
Ayan Biswas <ayanbiswas184@gmail.com>
2020-05-17 05:01:47 +0530

    reset password not done

---
+++ reset_passemail.php
@@ -0,0 +1,33 @@
+<?php
+require_once('config.php');
+session_start();
+
+$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+if (isset($_GET['token'])) {
+    $token = $_GET['token'];
+    $sql = "SELECT * FROM users WHERE token='$token' LIMIT 1";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+        $query = "UPDATE users SET verified=1 WHERE token='$token'";
+
+        if (mysqli_query($conn, $query)) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['role'] = $user['role'];
+            $_SESSION['verified'] = true;
+            $_SESSION['message'] = "Your email address has been verified successfully";
+            $_SESSION['type'] = 'alert-success';
+            header('location: index.php');
+            
+        }
+    } else {
+        echo "User not found!";
+    }
+} else {
+    echo "No token provided!";
+}
+?>
