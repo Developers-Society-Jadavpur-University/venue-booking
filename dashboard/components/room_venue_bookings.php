58c74832a2fefcbb11696cee55737f097ff2d9b9
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2020-05-16 23:45:32 +0530

    dashboard segmented into components

---
+++ dashboard/components/room_venue_bookings.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div class="col-xl-8 col-md-12">
+    <div class="card table-card">
+        <div class="card-header">
+            <h5>Room and Venue Bookings</h5>
+            <div class="card-header-right">
+                <ul class="list-unstyled card-option">
+                    <li><i class="feather icon-maximize full-card"></i></li>
+                    <li><i class="feather icon-minus minimize-card"></i>
+                    </li>
+                    <!-- <li><i class="feather icon-trash-2 close-card"></i></li> -->
+                </ul>
+            </div>
+        </div>
+        <div class="card-block">
+            <div class="table-responsive">
+                <table class="table table-hover  table-borderless">
+                    <thead>
+                        <tr>
+                            <th>
+
+                                Applicant
+                            </th>
+                            <th>Room/Venue</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Estimate</th>
+                            <th>Booking Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+
+                                <div class="d-inline-block align-middle">
+                                    <h6>Demo Applicant 1</h6>
+                                    <p class="text-muted m-b-0">Purpose :
+                                        Demo Purpose 1</p>
+                                </div>
+                            </td>
+                            <td>Gandhi Bhavan</td>
+                            <td>23 May 2020</td>
+                            <td>26 May 2020</td>
+                            <td>Rs. 36000</td>
+                            <td class="text-c-green">Confirmed</td>
+                        </tr>
+                        <tr>
+                            <td>
+
+                                <div class="d-inline-block align-middle">
+                                    <h6>Demo Applicant 2</h6>
+                                    <p class="text-muted m-b-0">Purpose :
+                                        Demo Purpose 2</p>
+                                </div>
+                            </td>
+                            <td>K.P. Basu Memorial Hall</td>
+                            <td>24 May 2020</td>
+                            <td>29 May 2020</td>
+                            <td>Rs. 12000</td>
+                            <td class="text-c-blue">Payment Processing</td>
+                        </tr>
+                        <tr>
+                            <td>
+
+                                <div class="d-inline-block align-middle">
+                                    <h6>Demo Applicant 3</h6>
+                                    <p class="text-muted m-b-0">Purpose :
+                                        Demo Purpose 3</p>
+                                </div>
+                            </td>
+                            <td>K.C.Roy Memorial Hall</td>
+                            <td>23 April 2020</td>
+                            <td>27 April 2020</td>
+                            <td>Rs. 40000</td>
+                            <td class="text-c-yellow">Payment Pending</td>
+                        </tr>
+                        <tr>
+                            <td>
+
+                                <div class="d-inline-block align-middle">
+                                    <h6>Demo Applicant 4</h6>
+                                    <p class="text-muted m-b-0">Purpose :
+                                        Demo Purpose 4</p>
+                                </div>
+                            </td>
+                            <td>TEQIP - 101</td>
+                            <td>23 May 2020</td>
+                            <td>24 May 2020</td>
+                            <td>Rs. 12000</td>
+                            <td class="text-c-pink">Approval Pending</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="text-center">
+                    <a href="#!" class=" b-b-primary text-primary">View all
+                        Bookings</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ dashboard/preloader.php
@@ -0,0 +1,43 @@
+<!-- Pre-loader start -->
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div class="theme-loader">
+        <div class="ball-scale">
+            <div class='contain'>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+                <div class="ring">
+                    <div class="frame"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Pre-loader end -->
+    </body>
+    </html>
