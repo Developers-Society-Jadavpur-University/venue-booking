c039c5d72427a6b5745fd3aac1954296f9c73b56
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2020-05-21 22:43:51 +0530

    header and sql issue fixed

--- db.sql
+++ db.sql
@@ -1,17 +1,17 @@
-CREATE TABLE IF NOT EXISTS`users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(100) NOT NULL,
- `role` varchar(50) NOT NULL,
- `email` varchar(100) NOT NULL,
- `verified` tinyint(1) NOT NULL DEFAULT '0',
- `token` varchar(255) DEFAULT NULL,
- `password` varchar(255) NOT NULL,
- PRIMARY KEY (`id`)
- )
+CREATE TABLE IF NOT EXISTS users (
+ id int(11) NOT NULL AUTO_INCREMENT,
+ username varchar(100) NOT NULL,
+ role varchar(50) NOT NULL,
+ email varchar(100) NOT NULL,
+ verified tinyint(1) NOT NULL DEFAULT '0',
+ token varchar(255) DEFAULT NULL,
+ password varchar(255) NOT NULL,
+ PRIMARY KEY (id)
+ );
  
- CREATE TABLE IF NOT EXISTS `otp_expiry` (
-`id` int(11) NOT NULL,
-  `otp` varchar(10) NOT NULL,
-  `is_expired` int(11) NOT NULL,
-  `create_at` datetime NOT NULL
+ CREATE TABLE IF NOT EXISTS otp_expiry (
+id int(11) NOT NULL,
+  otp varchar(10) NOT NULL,
+  is_expired int(11) NOT NULL,
+  create_at datetime NOT NULL
 )
--- logout.php
+++ logout.php
@@ -1,10 +1,5 @@
 <?php
 session_unset();
 session_destroy();
-/*unset($_SESSION['id']);
-unset($_SESSION['username']);
-unset($_SESSION['email']);
-unset($_SESSION['role']);
-unset($_SESSION['verify']);*/
 header("location: login.php");
 ?>
